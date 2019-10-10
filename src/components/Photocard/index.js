d56903f7405eba0dab5ca448fcb72ed6166de973
Elegido A dedo <elegidoadedo@MacBook-Pro-E.local>
2019-10-10 17:26:48 +0200

    destructuring

--- src/components/Photocard/index.js
+++ src/components/Photocard/index.js
@@ -1,33 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Article,ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md"
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/userNearScreen'
 
 const DEFAULT_IMAGE = "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png"
 
 export const Photocard = ({id, likes = 0, src = DEFAULT_IMAGE})=>{
-  const element = useRef(null)
-  const [ show, setShow ] = useState(false)
-  const [ liked, setLiked ] = useState(false)
-
-  useEffect(function(){
-    Promise.resolve(
-      typeof window.IntersectionObserver !== undefined
-      ? window.IntersectionObserver 
-      : import('intersection-observer')
-    ).then(() =>{
-        const observer = new window.IntersectionObserver(function (entries) {
-          const { isIntersecting } = entries[0]
-          console.log(isIntersecting)
-          if (isIntersecting) {
-            setShow(true)
-            observer.disconnect()
-          }
-        }, )
-        observer.observe(element.current)
-      } )
-   
-  }, [element])
-
+  const [show, element] = useNearScreen()
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
