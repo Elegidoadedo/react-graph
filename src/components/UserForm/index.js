2fefda30a97954d5294c799f1502d4c3816a3c8d
Elegido A dedo <elegidoadedo@MacBook-Pro-E.local>
2019-10-21 10:37:19 +0200

    added register form and mutation

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
+import { Form, Button, Input, Title, Error } from './styles'
 
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ error, disabled, onSubmit, title }) => {
   const  email = useInputValue('')
   const  password = useInputValue('')
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit({ email: email.value, password: password.value })
+  }
+
   return (
-    <form onSubmit={onSubmit}>
-      <input placeholder="Email" {...email} />
-      <input placeholder="Password" type="password" {...password} />
-      <button>Iniciar sesión</button>
-    </form>
+    <>
+      <Form disabled={disabled} onSubmit={handleSubmit}>
+        <Title>{ title }</Title>
+        <Input disabled={disabled} placeholder="Email" {...email} />
+        <Input disabled={disabled} placeholder="Password" type="password" {...password} />
+        <Button disabled={disabled} >{title}</Button>
+        {error && <Error>{ error }</Error>}
+      </Form>
+    </>
   )
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../containers/RegisterMutation'
 
-export const NotRegisteredUser = () => {
-
-  return (
-    <Context.Consumer>
-      {
-        ({ activateAuth }) =><UserForm onSubmit={activateAuth}/>
+export const NotRegisteredUser = () => (
+  <Context.Consumer>
+    {
+      ({ activateAuth }) => {
+        return <Fragment>
+          <RegisterMutation>
+            {
+              (register, { data, loading, error}) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  register({variables}).then(activateAuth)
+                }
+                const errorMsg = error && 'El usuario ya existe o hay algun problema'
+                return <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmit={onSubmit} />
+              }
+            }
+          </RegisterMutation>
+          <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
+        </Fragment>
       }
-    </Context.Consumer>
-  )
-}
+    }
+  </Context.Consumer>
+)
