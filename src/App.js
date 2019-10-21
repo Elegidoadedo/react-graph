2d5895d9ca40ae13b768e8008fbf8d70cee789f4
Elegido A dedo <elegidoadedo@MacBook-Pro-E.local>
2019-10-21 17:18:00 +0200

    routes finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Router } from '@reach/router'
+import React, { useContext } from 'react';
+import { Router, Redirect } from '@reach/router'
 import { GlobalStyles } from './styles/GlobalStyles'
-import Context from './Context'
+import { Context } from './Context'
 import { Logo } from './components/logo'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
@@ -9,31 +9,26 @@ import { Detail} from './pages/Detail'
 import { Favs} from './pages/Favs'
 import { User} from './pages/User'
 import { NotRegisteredUser} from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
 
 export const App = () => {
-
+  const { isAuth } = useContext(Context)
   return <div>
     <GlobalStyles />
     <Logo />
     <Router>
+      <NotFound default />
       <Home path='/' />
       <Home path='/pet/:id' />
       <Detail path='/detail/:detailId' />
+      {!isAuth && <NotRegisteredUser path='/login' />}
+      {!isAuth && <Redirect from='/favs' to='/login' noThrow/>}
+      {!isAuth && <Redirect from='/user' to='/login' noThrow/>}
+      {isAuth && <Redirect from='/login' to='/' noThrow/>}
+      <Favs path='/favs' />
+      <User path="/user" />
     </Router>
-      <Context.Consumer >
-        {({isAuth}) =>
-          isAuth
-            ? <Router>
-                <Favs path='/favs' />
-                <User path="/user" /> 
-            </Router>
-            : <Router>
-                <NotRegisteredUser path='/favs' />
-                <NotRegisteredUser path='/user' />
-            </Router>
-        }
-      </Context.Consumer>
     <NavBar />
   </div>
 }
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotFound = () => (
+  <h1>No se ha encontrado esta página :(</h1>
+)
